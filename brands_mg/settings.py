01fe824274fdeb7f4c3734e7f44f7d714873084a
mari-code-l <maridel.arcagua@gmail.com>
2021-08-30 23:53:25 +0000

    Remove sercret key and set debug

--- brands_mg/settings.py
+++ brands_mg/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@j+%xzur$nko&d0ll@v+*g-)-&%m3@o42xd3-u!u%e+j4ig6q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mg-brands.herokuapp.com', 'localhost']
 
