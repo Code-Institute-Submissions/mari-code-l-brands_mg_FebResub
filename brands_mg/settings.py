458498089aae7cadffea138118c033e14a2c35d8
mari-code-l <maridel.arcagua@gmail.com>
2021-09-13 16:37:27 +0000

    added cache control

--- brands_mg/settings.py
+++ brands_mg/settings.py
@@ -182,6 +182,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'mg-brands'
     AWS_S3_REGION_NAME = 'eu-central-1'
