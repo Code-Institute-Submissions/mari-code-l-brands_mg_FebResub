323da47cb8106103c4979d246f7a90dfb10a2e53
mari-code-l <maridel.arcagua@gmail.com>
2021-10-01 23:10:45 +0000

    debug success checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -20,7 +20,6 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
