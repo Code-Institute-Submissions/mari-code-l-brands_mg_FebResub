27f04d0111e11dc615019f391ed483dd010455f0
mari-code-l <maridel.arcagua@gmail.com>
2021-10-01 21:16:45 +0000

    Added checkout button functionality

--- brands_mg/settings.py
+++ brands_mg/settings.py
@@ -213,4 +213,4 @@ STRIPE_CURRENCY = 'php'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'maridel.arcagua@gmail.com'
+DEFAULT_FROM_EMAIL = 'madelvonlove@icloud.com'
