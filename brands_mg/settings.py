9ca07c04a03c7e2e2a1e879f4bf6457b89ae17f5
mari-code-l <maridel.arcagua@gmail.com>
2021-12-05 15:45:09 +0000

    Changed email

--- brands_mg/settings.py
+++ brands_mg/settings.py
@@ -209,7 +209,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'madelvonlove@icloud.com'
+    DEFAULT_FROM_EMAIL = 'maridel.arcagua@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
