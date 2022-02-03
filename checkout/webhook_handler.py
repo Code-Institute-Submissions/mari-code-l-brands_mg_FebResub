0f42b540e72522ea1e84df1a89d987a0a685aa8d
mari-code-l <maridel.arcagua@gmail.com>
2022-02-03 18:02:43 +0000

    Debug set into False

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -31,7 +31,7 @@ class StripeWH_Handler:
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
+            [cust_email],
         )
 
     def handle_event(self, event):
